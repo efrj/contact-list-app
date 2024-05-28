0d59f886aae2fea9680b16717903e76082daebaf
Edivaldo Reis <efrj@hotmail.com>
2024-05-27 23:03:46 -0300

    Added contacts migration

---
+++ db/migrate/20240528015055_create_contacts.rb
@@ -0,0 +1,18 @@
+class CreateContacts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :cpf
+      t.string :phone
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.float :latitude
+      t.float :longitude
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

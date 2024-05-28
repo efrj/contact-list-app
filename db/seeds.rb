64be10bbbb39145308628f9b6f3a1a12fb0be385
Edivaldo Reis <efrj@hotmail.com>
2024-05-28 14:55:31 -0300

    Updated DB Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+User.destroy_all
+Contact.destroy_all
+
+users = []
+5.times do
+  users << User.create!(
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+
+users.each do |user|
+  10.times do
+    address = Faker::Address.street_address
+    city = Faker::Address.city
+    state = Faker::Address.state
+    zip = Faker::Address.zip
+    full_address = "#{address}, #{city}, #{state}, #{zip}"
+    
+    coordinates = Geocoder.search(full_address).first&.coordinates || [Faker::Address.latitude, Faker::Address.longitude]
+    
+    Contact.create!(
+      name: Faker::Name.name,
+      cpf: CPF.generate(true),
+      phone: Faker::PhoneNumber.phone_number,
+      address: address,
+      city: city,
+      state: state,
+      zip: zip,
+      latitude: coordinates[0],
+      longitude: coordinates[1],
+      user: user
+    )
+  end
+end
+
+puts "Seed completed! Created #{User.count} users and #{Contact.count} contacts."
